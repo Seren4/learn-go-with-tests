f4d703d75eeb08de78047f23a9520464164a1164
Serena CILIBERTI <25780127+Seren4@users.noreply.github.com>
2025-11-17 11:24:10 +0100

    refacto

--- helloworld/hello_test.go
+++ helloworld/hello_test.go
@@ -6,18 +6,19 @@ func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Chris")
 		expected := "Hello, Chris"
+		assertCorrectMessage(t, got, expected)
 
-		if got != expected {
-			t.Errorf("got %q, want %q", got, expected)
-		}
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
-		expected := "Hello, World"
-
-		if got != expected {
-			t.Errorf("got %q want %q", got, expected)
-		}
+		expected := "Hello, Worldd"
+		assertCorrectMessage(t, got, expected)
 	})
 
+}
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
 }
