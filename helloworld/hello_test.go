cb4f1403795583e1faaadb9ef69fb328e6fb898d
Serena CILIBERTI <25780127+Seren4@users.noreply.github.com>
2025-11-17 10:54:54 +0100

    hello world

---
+++ helloworld/hello_test.go
@@ -0,0 +1,13 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	got := Hello()
+	expected := "Hello, world!"
+
+	if got != expected {
+		t.Errorf("got %q, want %q", got, expected)
+	}
+
+}
